6bf1623919e0d84edd1dbe799b5d36e9cbf70a24
〣莣パ記 <598944511@qq.com>
2017-03-30 17:25:44 +0800

    iptables监控 字符集修改
    iptables监控 字符集修改

--- zabbix-iptables/iptables.sh
+++ zabbix-iptables/iptables.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 #iptables 监控脚本
-#2016/11-18
-function tcp {  
+#2017-03-30
+function tcp {
 sudo iptstate --single | grep tcp | wc -l
-}  
-function tcp-syn {  
+}
+function tcp-syn {
 sudo iptstate --single | grep SYN | wc -l
-}  
-function tcp-timewait {  
+}
+function tcp-timewait {
 sudo iptstate --single | grep TIME_WAIT | wc -l
-}  
-function tcp-established {  
+}
+function tcp-established {
 sudo iptstate --single | grep ESTABLISHED | wc -l 
-}  
-function tcp-close {  
+}
+function tcp-close {
 sudo iptstate --single | grep CLOSE | wc -l
-}  
-function udp {  
+}
+function udp {
 sudo iptstate --single | grep udp | wc -l
-}  
-function icmp {  
+}
+function icmp {
 sudo iptstate --single | grep icmp | wc -l
 }
-function all {  
-sudo iptstate --single | wc -l  
+function all {
+sudo iptstate --single | wc -l
 }
-# Run the requested function  
 $1
